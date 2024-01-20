201b8a944b592582e862e6e44de33f5ff393159e
Alicia Saci <lyciiasci@gmail.com>
2024-01-20 23:35:50 +0100

    first commit

---
+++ Header.php
@@ -0,0 +1,10 @@
+<div class="header">
+ <div class="logo">
+  <span>Poub'UL</span>
+ </div>
+ <a href="./index.php"  class="link-dark">Accueil</a>
+ <div class="d-grid gap-2 d-md-block header-btns">
+  <a href="./signup.php" class="btn btn-primary" type="button">S'inscrire</a>
+  <a href="./login.php" class="btn btn-primary" type="button">Se connecter</a>
+ </div>
+</div>
---
+++ signup.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="./style/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<body>
+ <?php include('./Header.php'); ?>
+ <div class="login-img">
+  <img src="./assets/logo.png" />
+ </div>
+ <div class="login">
+  <span class="se-connecter">S'inscrire</span>
+  <div class="mb-3">
+   <label for="exampleFormControlInput1" class="form-label">Prenom</label>
+   <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Prenom">
+  </div>
+  <div class="mb-3">
+   <label for="exampleFormControlInput1" class="form-label">Nom</label>
+   <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nom">
+  </div>
+  <div class="mb-3">
+   <label for="exampleFormControlInput1" class="form-label">Email</label>
+   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="mb-3">
+   <label for="exampleFormControlInput1" class="form-label">Mot de passe</label>
+   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="********">
+  </div>
+  <a href="" class="btn btn-primary" type="button">S'inscrire</a>
+ </div>
+</body>
